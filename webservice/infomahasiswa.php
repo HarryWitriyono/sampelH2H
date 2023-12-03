6d25915890a7fa0a685b1c68417da6b2c3ec5e8c
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2023-12-04 06:54:12 +0700

    Add files via upload

---
+++ webservice/infomahasiswa.php
@@ -0,0 +1,15 @@
+<?php 
+if (isset($_GET['NPM'])) {
+    include('../servera/koneksi.db.php');
+    $NPM=filter_var($_GET['NPM'],FILTER_SANITIZE_STRING);
+    $sql="select NPM,NamaMahasiswa from mahasiswa where NPM='".$NPM."'";
+    $q=mysqli_query($koneksi,$sql);
+    $r=mysqli_fetch_array($q);
+    $hasil=array();
+    //do {
+        $hasil= $r;
+    //}while($r=mysqli_fetch_array($q));
+    $hasil=json_encode($hasil);
+    echo $hasil;
+    mysqli_close($koneksi);
+} 
