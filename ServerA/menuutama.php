b859d7d8bad0f3109407adecfbf0a2e0385690e8
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2023-12-04 06:05:26 +0700

    Add files via upload

---
+++ ServerA/menuutama.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 4, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h3 class="text-center text-success">
+			Sistem Pembayaran Akademik
+			</h3>
+			<ul class="nav">
+				<li class="nav-item">
+					<a class="nav-link active" href="menuutama.php">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Cek Pembayaran</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Daftar Tagihan</a>
+				</li>
+				<li class="nav-item dropdown">
+					 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Laporan</a>
+					<div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdownMenuLink">
+						 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+						<div class="dropdown-divider">
+						</div> <a class="dropdown-item" href="#">Separated link</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown ml-md-auto">
+					 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">System</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+						 <a class="dropdown-item" href="#">Ubah Password</a> 
+						 <a class="dropdown-item" href="#">Log Report</a>
+						<div class="dropdown-divider">
+						</div> <a class="dropdown-item" href="#">Logout</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
